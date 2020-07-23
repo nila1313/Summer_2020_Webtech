4e01fa02d637a564ed8186482682a90c20c4019a
nila1313 <chaity13x@gmail.com>
2020-07-23 16:16:07 +0600

    Email

---
+++ PHP_TASK_03/two.php
@@ -0,0 +1,33 @@
+<?php
+
+   $email=$_GET['email'];
+   $condition = False;
+   $positionAt =strpos($email, '@');
+   $positionDotCom = strpos($email, ".com");
+
+
+  if($email=="")
+  {
+    echo "Please Enter Your Email Address ";
+  }
+  elseif( $positionAt==True && $positionDotCom==True)
+    echo "Thank you";
+
+  else
+  {
+    echo "Try again",$email;
+  }
+
+?>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <form>
+    Email <br>
+    <input type="email" name="email" placeholder="sample@example.com"><br>
+    <input type="submit" name="submit">
+  </form>
+</body>
+</html>
