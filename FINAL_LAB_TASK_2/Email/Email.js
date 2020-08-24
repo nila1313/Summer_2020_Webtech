7777741e3399723f1fcc2e26cf62474042b73daa
nila1313 <chaity13x@gmail.com>
2020-08-24 20:49:39 +0600

    Email.php

--- FINAL_LAB_TASK_2/Email/Email.js
+++ FINAL_LAB_TASK_2/Email/Email.js
@@ -5,14 +5,14 @@ function validate()
 	var email=document.getElementById('email').value;
 	if (email=="") 
 	{
-		document.getElementById('emailmsg').innerHTML="*Can not be empty";
+		document.getElementById('emailMsg').innerHTML="*Can not be empty";
 		return false;
 	}	
 }
 
 function remover()
 {
-	document.getElementById('emailmsg').innerHTML="";
+	document.getElementById('emailMsg').innerHTML="";
 }
 
 function mustNeed(){
