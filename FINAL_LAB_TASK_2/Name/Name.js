a3a9eafd1da3198e2b2906b515f18e60437f66e2
nila1313 <chaity13x@gmail.com>
2020-08-24 22:08:59 +0600

    Update Name.js

--- FINAL_LAB_TASK_2/Name/Name.js
+++ FINAL_LAB_TASK_2/Name/Name.js
@@ -1,61 +1,53 @@
 
 "use strict"
 
-var validation=false;
+
 function nameValidate(){
+  var validation=false;
   
   var username = document.getElementById('username').value; 
  
-
+ 
   if(username==""){
    
     document.getElementById('userMsg').innerHTML="*this field is required";
-    window.validation=false;
+    validation=false;
 
   }
 
 
-  else if(username.length<2){
+  else if(userName.split(" ").length<2){
    
     document.getElementById('userMsg').innerHTML="*At least two characters are needed";
-    window.validation=false;
+    validation=false;
 
   }
-  else if(username.length<2 && username>='0' && username<='9')
+  else if(userName.split(" ").length<2 && username>='0' && username<='9')
     {   
          document.getElementById("userMsg").innerHTML="*Must be starts with letter ";  
-        window.validation=false;
+        validation=false;
 
     }
-
-// else if(username=='&'|| username=='$')
-//     {   
-//          document.getElementById("userMsg").innerHTML="*Can Contains A-Z or a-z or dot(.) or dash(-) ";  
-//         window.validation=false;
-
-//     }
-   
-   else if( username>='A' && username<='Z' || username>='a'&& username<='z' || username=='.') || username=='-')
+else if( (username>='A' && username<='Z') || (username>='a'&& username<='z') || username=='.' || username=='-')
     {   
         document.getElementById("userMsg").innerHTML="*Can Contains A-Z or a-z or dot(.) or dash(-) "; 
-        window.validation=true;
+        validation=true;
 
     }
-
- 
-}
+    return validation;
+  }
 
 function Validate()
 {
-	nameValidate();
-	if(window.validation==true)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+  var val=nameValidate();
+  if(val==true)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 
 }
 
