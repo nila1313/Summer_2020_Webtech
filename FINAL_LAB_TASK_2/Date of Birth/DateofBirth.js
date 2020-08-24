b948c85515c4837835e8160b5bb0aefce5edf654
nila1313 <chaity13x@gmail.com>
2020-08-24 22:33:22 +0600

    DateofBirth.js

---
+++ FINAL_LAB_TASK_2/Date of Birth/DateofBirth.js
@@ -0,0 +1,31 @@
+
+"use strict"
+
+function validate()
+{
+	var day=document.getElementById('day').value;
+	var month=document.getElementById('month').value;
+	var year=document.getElementById('year').value;
+	if (day!="" && month!="" && year!="") 
+	{
+		if(day>0 && day<32 && month>0 && month<13 && year>1899 && year<2017)
+		{
+			return true;
+		}
+		else
+		{
+			document.getElementById('dateofbirthMsg').innerHTML="day range 0 to 31 and month range 1 to 12 and year range 1900 to 2016";
+			return false;
+		}
+	}
+	else
+	{
+		document.getElementById('dateofbirthMsg').innerHTML="Please fill all the boxes";
+		return false;
+	}
+	
+}
+function remover()
+{
+	document.getElementById('dateofbirthMsg').innerHTML="";
+}
