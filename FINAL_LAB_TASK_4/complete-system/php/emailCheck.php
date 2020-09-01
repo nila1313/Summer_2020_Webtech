c7237041a7df4f94bae74e95d1eabd75b274ca1c
nila1313 <chaity13x@gmail.com>
2020-09-01 09:29:50 +0600

    email.check

---
+++ FINAL_LAB_TASK_4/complete-system/php/emailCheck.php
@@ -0,0 +1,18 @@
+<?php 
+	session_start();
+	//require_once('../php/session_header.php');
+	require_once('../service/userService.php');
+
+
+	//add user
+	if(isset($_POST['email'])){
+
+		$status=checkEmail($_POST['email']);
+		if($status)
+		{
+			echo "This email already there";
+		}		
+	}
+
+
+?>
